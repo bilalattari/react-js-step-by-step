882b0ed3cdf9fd26f8f4ee3c0ed5fa4d1c104e40
Bilal raza <attari1235@gmail.com>
2024-09-11 09:41:19 +0500

    Material UI with React Js

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,175 @@
-import "./App.css";
-import Button from "./components/Button";
-import Features from "./components/Features";
-import Footer from "./components/Footer";
+import {
+  Avatar,
+  Box,
+  Button,
+  CircularProgress,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Image, SearchOutlined } from "@mui/icons-material";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Testimonials from "./components/Testimonials";
-import landingPageData from "./constant/webData";
+import { useEffect, useState } from "react";
+import { Masonry } from "@mui/lab";
 
 function App() {
-  const { header, hero, features, testimonials } = landingPageData;
-  const user = {
-    isLogin: false,
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [chosen, setChosen] = useState(null);
+
+  useEffect(() => {
+    getImagesFromUnsplash();
+  }, []);
+
+  const getImagesFromUnsplash = () => {
+    fetch(
+      "https://api.unsplash.com/photos?page=1&client_id=oTO49XdAaqBeNW1n3tO0fOX6Qkhi_tC1UK4NvmQql-Q&per_page=30"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setImages(res);
+        setLoading(false);
+      })
+      .catch((err) => setLoading(false));
   };
+
+  const searchPhotosFromUnsplash = () => {
+    if (search) {
+      fetch(
+        `https://api.unsplash.com/search/photos?page=1&query=${search}&client_id=oTO49XdAaqBeNW1n3tO0fOX6Qkhi_tC1UK4NvmQql-Q&per_page=30`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          // console.log("search ka response kya araha he=>", res);
+          setImages(res.results);
+          setLoading(false);
+        })
+        .catch((err) => setLoading(false));
+    }
+  };
+
+  console.log(chosen);
+
   return (
     <div>
-      <Header header={header} user={user} />
-      <Hero hero={hero} />
-      <Features features={features} />
-      <Testimonials testimonials={testimonials} />
-      <Footer header={header} />
+      <Modal
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        open={showModal}
+        onClose={() => setShowModal(false)}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            width: "80%",
+            overflow: "hidden",
+          }}
+        >
+          <img
+            width={"50%"}
+            style={{
+              maxHeight: 700,
+            }}
+            className="contain"
+            src={chosen?.urls?.regular}
+          />
+
+          <Box display={"block"} width={"50%"} padding={2} bgcolor={"white"}>
+            <Typography fontWeight={"bold"} variant="h4">
+              {chosen?.alt_description}
+            </Typography>
+            <Box
+              display={"flex"}
+              sx={{
+                marginTop: 2,
+                marginBottom: 2,
+              }}
+            >
+              <Avatar
+                sizes="medium"
+                src={
+                  chosen?.sponsorship?.sponsor?.profile_image?.medium ||
+                  chosen?.user?.profile_image?.medium
+                }
+              />
+              <Box marginLeft={1}>
+                <Typography>
+                  {chosen?.sponsorship?.sponsor?.name || chosen?.user?.name}
+                </Typography>
+                <Typography>
+                  Total Photos :{" "}
+                  {chosen?.sponsorship?.sponsor?.total_photos ||
+                    chosen?.user?.total_photos}
+                </Typography>
+              </Box>
+            </Box>
+
+            <Typography fontWeight={"medium"} variant="h6">
+              Likes : {chosen?.likes}
+            </Typography>
+          </Box>
+        </Box>
+      </Modal>
+      <Header />
+      <div className="container mx-auto">
+        <Box
+          display={"flex"}
+          marginTop={2}
+          marginBottom={2}
+          gap={2}
+          justifyContent={"space-between"}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Search Your Images"
+            fullWidth
+            color="secondary"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            variant="outlined"
+          />
+          <Button
+            startIcon={<SearchOutlined />}
+            disabled={search === "" || search.length < 3}
+            variant="outlined"
+            color="secondary"
+            onClick={searchPhotosFromUnsplash}
+          >
+            Search
+          </Button>
+        </Box>
+        {loading ? (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: 10,
+            }}
+          >
+            <CircularProgress color="secondary" size={50} />
+          </Box>
+        ) : null}
+
+        <Masonry columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={2}>
+          {images.map((item) => (
+            <img
+              onClick={() => {
+                setShowModal(true);
+                setChosen(item);
+              }}
+              key={item.id}
+              src={item.urls.regular}
+            />
+          ))}
+        </Masonry>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,161 @@
-import Button from "./Button";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
 
-function Header({ header, user }) {
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <img src={header.logo} className="h-10 w-10 rounded-full" />
-          <span className="ml-3 text-xl">{header.title}</span>
-        </a>
-        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">Contact Us</a>
-          <a className="mr-5 hover:text-gray-900">About Us</a>
-        </nav>
-        <Button label={ user.isLogin ? "Logout" : "Login"} />
-      </div>
-    </header>
+    <AppBar position="static" color="secondary" enableColorOnDark>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              // display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              // fontWeight: 700,
+              // letterSpacing: ".3rem",
+              // color: "inherit",
+              // textDecoration: "none",
+            }}
+          >
+            Image Gallery
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ textAlign: "center" }}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: "center" }}>
+                    {setting}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
-
 export default Header;
